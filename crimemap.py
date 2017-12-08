173b35101e4fcf71ef8f1745195784f7372233b6
Edi Santoso <repopamor@gmail.com>
2017-12-09 06:27:08 +0700

    validating and sanitizing

--- crimemap.py
+++ crimemap.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import db_config
 import json
+import datetime
+import dateparser
+import string
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -13,13 +16,17 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging','break-in']
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html",
+            crimes=crimes,
+            categories=categories,
+            error_message=error_message)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -42,12 +49,34 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError as ve:
+        print ve
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+# helper to validate date
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+# XSS protection
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 if __name__ == '__main__':
