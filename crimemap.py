e590a3bd9dda4b6bc008efcfa1d3c12b603f7f66
Edi Santoso <repopamor@gmail.com>
2017-12-08 10:13:47 +0700

    add route to clear all data

--- crimemap.py
+++ crimemap.py
@@ -18,12 +18,19 @@ def home():
         data = None
     return render_template("home.html", data=data)
 
-
 @app.route("/add", methods=["POST"])
 def add():
     try:
         data = request.form.get("userinput")
         DB.add_input(data)
+    except Exception as e:
+        print e
+    return home()
+
+@app.route("/clear")
+def clear():
+    try:
+        DB.clear_all()
     except Exception as e:
         print e
     return home()
